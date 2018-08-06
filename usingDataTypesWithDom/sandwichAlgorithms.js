9256ad74f802e3c072681aa2bedcd930b7df97fe
“aztinajmai” <aztinajmai@gmail.com>
2018-08-06 15:08:23 -0400

    displayNamesAssignment

---
+++ usingDataTypesWithDom/sandwichAlgorithms.js
@@ -0,0 +1,10 @@
+makeSandwichWith _____
+	Get one slice of bread.
+	Add _____.
+	Put a slice of bread on top.
+
+function makeSandwhichWith(filling) {
+	Get one slice of bread;
+	Add filling;
+	Put a slice of bread on top;
+	}
